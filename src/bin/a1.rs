c7e422ba1a32e256bb7af0e95da42ab00e89339e
ashanuoc <59000312+ashanuoc@users.noreply.github.com>
2024-11-04 21:10:04 -0500

    my first program in rust

--- src/bin/a1.rs
+++ src/bin/a1.rs
@@ -8,5 +8,19 @@
 // * Use a function to display your last name
 // * Use the println macro to display messages to the terminal
 
-fn main() {}
+
+// * Use a function to display your first name
+fn first_name(){
+    println!("Jayson");
+}
+
+// * Use a function to display your last name
+fn last_name(){
+    println!("Lennon");
+}
+fn main() {
+    first_name();
+    last_name();
+
+}
 
