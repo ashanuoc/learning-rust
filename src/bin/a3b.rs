be37dc2265ab5a18497ffa0c650566a0e9ae7814
ashanuoc <59000312+ashanuoc@users.noreply.github.com>
2024-11-05 20:48:42 -0500

    if-else-if-else

--- src/bin/a3b.rs
+++ src/bin/a3b.rs
@@ -9,5 +9,16 @@
 // * Use an if..else if..else block to determine which message to display
 // * Use the println macro to display messages to the terminal
 
-fn main() {}
+fn main() {
+    let var = 6;
+    if var > 5{
+        println!(">5");
+    }
+    else if var == 5 {
+        println!("=5");
+    }
+    else {
+        println!(">=5");
+    }
+}
 
