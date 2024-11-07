c4952b63a0a0542585dd043e2f0abd9994e35948
ashanuoc <59000312+ashanuoc@users.noreply.github.com>
2024-11-06 20:05:25 -0500

    while

--- src/bin/a6.rs
+++ src/bin/a6.rs
@@ -10,5 +10,11 @@
 // * Print the variable within the while loop
 // * Do not use break to exit the loop
 
-fn main() {}
+fn main() {
+    let mut n = 5;
+    while n > 0{
+        println!("{:?}", n);
+        n -=1;
+    }
+}
 
